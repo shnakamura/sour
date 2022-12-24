075917bf39988411542be7a65f2c2589883e6532
naka <joaovtnakamura@gmail.com>
2022-12-24 16:16:00 -0300

    Minor refactoring

---
+++ Interpreter.cs
@@ -0,0 +1,114 @@
+﻿namespace BrainfuckInterpreter;
+
+public sealed class Interpreter
+{
+    public readonly string FilePath;
+    public readonly byte[] Memory;
+
+    private int memoryPointer;
+
+    public Interpreter(string filePath) {
+        if (string.IsNullOrEmpty(filePath)) {
+            throw new ArgumentNullException(nameof(filePath));
+        }
+
+        Memory = new byte[30000];
+        FilePath = filePath;
+    }
+
+    public void Interpret() {
+        char[] fileContent = File.ReadAllText(FilePath).ToCharArray();
+
+        for (int i = 0; i < fileContent.Length; i++) {
+            switch (fileContent[i]) {
+                case '>':
+                    memoryPointer++;
+
+                    if (memoryPointer > Memory.Length) {
+                        memoryPointer = 0;
+                    }
+
+                    break;
+
+                case '<':
+                    memoryPointer--;
+
+                    if (memoryPointer < 0) {
+                        memoryPointer = Memory.Length - 1;
+                    }
+
+                    break;
+
+                case '+':
+                    Memory[memoryPointer]++;
+
+                    break;
+
+                case '-':
+                    Memory[memoryPointer]--;
+
+                    break;
+
+                case '.':
+                    Console.Write(Memory[memoryPointer]);
+
+                    break;
+
+                case ',':
+                    Memory[memoryPointer] = Convert.ToByte(Console.ReadKey());
+
+                    break;
+
+                case '[':
+                    if (Memory[memoryPointer] == 0) {
+                        int skip = 0;
+                        int ptr = i + 1;
+
+                        while (fileContent[ptr] != ']' || skip > 0) {
+                            switch (fileContent[ptr]) {
+                                case '[':
+                                    skip++;
+
+                                    break;
+
+                                case ']':
+                                    skip--;
+
+                                    break;
+                            }
+
+                            ptr++;
+                            i = ptr;
+                        }
+                    }
+
+                    break;
+
+                case ']':
+                    if (Memory[memoryPointer] != 0) {
+                        int skip = 0;
+                        int ptr = i - 1;
+
+                        while (fileContent[ptr] != '[' || skip > 0) {
+                            switch (fileContent[ptr]) {
+                                case '[':
+                                    skip--;
+
+                                    break;
+
+                                case ']':
+                                    skip++;
+
+                                    break;
+                            }
+
+                            ptr--;
+                            i = ptr;
+                        }
+                    }
+
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace BrainfuckInterpreter;
+
+public static class Program
+{
+    public static void Main(string[] args) {
+        if (!args.Any()) {
+            Console.WriteLine("Could not find any files.");
+
+            return;
+        }
+
+        foreach (string filePath in args) {
+            StringBuilder builder = new();
+
+            builder.Append(Path.GetFileName(filePath));
+            builder.Append(' ');
+            builder.Append('=', 50 - Path.GetFileName(filePath).Length);
+
+            Console.WriteLine(builder);
+            Console.WriteLine();
+
+            new Interpreter(filePath).Interpret();
+
+            builder = new StringBuilder();
+            builder.Append('=', 51);
+            builder.AppendLine();
+            builder.AppendLine();
+
+            Console.WriteLine(builder);
+            Console.WriteLine();
+        }
+
+        Console.ReadKey();
+    }
+}
