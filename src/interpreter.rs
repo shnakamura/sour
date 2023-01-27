c143ddd50947ee406eddc05479f092c6101f74e5
naka <joaovtnakamura@gmail.com>
2023-01-27 20:44:40 -0300

    Safety reviews

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,93 +1,90 @@
-use std::path::Path;
-use std::fs;
-
-use crate::SourArgs;
-
-pub struct Interpreter {
-    pub args: SourArgs
-}
+pub struct Interpreter { }
 
 impl Interpreter {
-    pub fn interpret(&self) {
-        let mut memory_buffer: Vec<u8> = vec![0u8; self.args.size];
+    pub fn interpret(content: String, size: usize, raw: bool) {
+        let mut memory_buffer: Vec<u8> = vec![0u8; size];
         let mut memory_pointer = 0;
         let mut char_pointer = 0;
-        let mut brainfuck_content = self.args.content.clone();
-   
-        if Path::new(&brainfuck_content).exists() {
-            brainfuck_content = fs::read_to_string(&brainfuck_content).unwrap();
-         }
 
-        while char_pointer < brainfuck_content.len() {
-            let char = brainfuck_content.chars().nth(char_pointer).unwrap();
-
-            match char {
-                '>' => {
-                    if memory_pointer < self.args.size {
-                        memory_pointer += 1;
-                    }
-                },
-                '<' => {
-                    if memory_pointer > 0 {
-                        memory_pointer -= 1;
-                    }
-                },
-                '+' => {
-                    if memory_buffer[memory_pointer] < u8::MAX {
-                        memory_buffer[memory_pointer] += 1;
-                    }
-                },
-                '-' => {
-                    if memory_buffer[memory_pointer] > 0 {
-                        memory_buffer[memory_pointer] -= 1;
-                    }
-                },
-                '.' => {
-                    if self.args.raw {
-                        print!("{}", memory_buffer[memory_pointer]);
-                    }
-                    else {
-                        print!("{}", memory_buffer[memory_pointer] as char);
-                    }
-                },
-                ',' => {
-                    let mut input_text = String::new();
-                    std::io::stdin().read_line(&mut input_text).unwrap();
-                    memory_buffer[memory_pointer] = input_text.trim().parse::<u8>().unwrap();
-                },
-                '[' => {
-                    if memory_buffer[memory_pointer] == 0 {
-                        let mut loops = 1;
-
-                        while loops > 0 {
-                            char_pointer += 1;
-
-                            match brainfuck_content.chars().nth(char_pointer).unwrap() {
-                                '[' => loops += 1,
-                                ']' => loops -= 1,
-                                _ => {}
+        while char_pointer < content.len() {
+            if let Some(char) = content.chars().nth(char_pointer) {
+                match char {
+                    '>' => {
+                        if memory_pointer < size {
+                            memory_pointer += 1;
+                        }
+                    },
+                    '<' => {
+                        if memory_pointer > 0 {
+                            memory_pointer -= 1;
+                        }
+                    },
+                    '+' => {
+                        if memory_buffer[memory_pointer] < u8::MAX {
+                            memory_buffer[memory_pointer] += 1;
+                        }
+                    },
+                    '-' => {
+                        if memory_buffer[memory_pointer] > 0 {
+                            memory_buffer[memory_pointer] -= 1;
+                        }
+                    },
+                    '.' => {
+                        if raw {
+                            print!("{}", memory_buffer[memory_pointer]);
+                        }
+                        else {
+                            print!("{}", memory_buffer[memory_pointer] as char);
+                        }
+                    },
+                    ',' => {
+                        let mut input_text = String::new();
+    
+                        match std::io::stdin().read_line(&mut input_text) {
+                            Ok(_) => match input_text.trim().parse::<u8>() {
+                                Ok(parsed) => memory_buffer[memory_pointer] = parsed,
+                                Err(error) => println!("Could not parse the user input {:?}", error)
+                            },
+                            Err(error) => println!("Could not read user input {:?}", error)
+                        }
+                    },
+                    '[' => {
+                        if memory_buffer[memory_pointer] == 0 {
+                            let mut loops = 1;
+    
+                            while loops > 0 {
+                                char_pointer += 1;
+    
+                                match char {
+                                    '[' => loops += 1,
+                                    ']' => loops -= 1,
+                                    _ => {}
+                                }
                             }
                         }
-                    }
-                },
-                ']' => {
-                    if memory_buffer[memory_pointer] != 0 {
-                        let mut loops = 1;
-
-                        while loops > 0 {
-                            char_pointer -= 1;
-
-                            match brainfuck_content.chars().nth(char_pointer).unwrap() {
-                                '[' => loops -= 1,
-                                ']' => loops += 1,
-                                _ => {}
+                    },
+                    ']' => {
+                        if memory_buffer[memory_pointer] != 0 {
+                            let mut loops = 1;
+    
+                            while loops > 0 {
+                                char_pointer -= 1;
+    
+                                match char {
+                                    '[' => loops -= 1,
+                                    ']' => loops += 1,
+                                    _ => {}
+                                }
                             }
                         }
-                    }
-                },
-                _ => { }
+                    },
+                    _ => { }
+                }
+            } 
+            else {
+                println!("Could not read the {}th char of the given content", char_pointer)
             }
-
+            
             char_pointer += 1;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,36 @@
+use std::fs;
+use std::path::Path;
 use clap::Parser;
-use args::SourArgs;
 use interpreter::Interpreter;
 
-mod args;
 mod interpreter;
 
-pub fn main() {
-   let interpreter = Interpreter {
-        args: SourArgs::parse()
-   };
+#[derive(Parser)]
+#[command(author, version)]
+pub struct SourArgs {
+    /// The brainfuck content or path to brainfuck file.
+    #[arg()]
+    pub content: String,
 
-   interpreter.interpret();
+    /// The memory buffer size.
+    #[arg(short, long, default_value_t = 30000)]
+    pub size: usize,
+
+    /// Whether to output raw bytes or encode to ASCII.
+    #[arg(short, long, default_value_t = false)]
+    pub raw: bool
+}
+
+pub fn main() {
+   let args = SourArgs::parse();
+
+   if Path::new(&args.content).exists() {
+      match fs::read_to_string(args.content) {
+         Ok(content) => Interpreter::interpret(content, args.size, args.raw),
+         Err(error) => panic!("Could not read the file {:?}", error)
+      } 
+   }
+   else {
+      Interpreter::interpret(args.content, args.size, args.raw);
+   }
 }
